5fd66c341e1e9a25b70d3ab5619563405480510d
PrasanthBalaraman <prsnth33@gmail.com>
2018-10-28 22:51:16 +0530

    added tensorflow autodiff and scaled input features

--- QuestionAnswering/PrasanthBalaraman/TensorflowPractice/ImplementingGradientDescent.py
+++ QuestionAnswering/PrasanthBalaraman/TensorflowPractice/ImplementingGradientDescent.py
@@ -2,11 +2,19 @@ import numpy as np
 import tensorflow as tf
 from sklearn.datasets import fetch_california_housing 
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_absolute_error
 
 # data preparation
 housing = fetch_california_housing()
 train_X, test_X, train_y, test_y = train_test_split(housing.data, housing.target, test_size=0.2, random_state=42)
+
+# scaling the data as gradient descent performs faster when all features are in same scale
+train_scaler = StandardScaler()
+train_x = train_scaler.fit_transform(train_X)
+test_scalar = StandardScaler() 
+test_X = test_scalar.fit_transform(test_X)
+
 train_X_plus_bias = np.c_[np.ones((train_X.shape[0], 1)), train_X]
 test_X_plus_bias = np.c_[np.ones((test_X.shape[0], 1)), test_X]
 train_y_reshaped = train_y.reshape(-1, 1)
@@ -14,8 +22,8 @@ test_y_reshaped = test_y.reshape(-1, 1)
 print("Shape after adding bias and reshaping y: ", train_X_plus_bias.shape, test_X_plus_bias.shape, train_y_reshaped.shape, test_y_reshaped.shape)
 
 # defining hyperparameters
-epochs = 10000
-learning_rate = 0.00000001
+epochs = 100000
+learning_rate = 0.000001   # for gradients computed manually please set learning rate minimum 0.00000001 
 
 # defining the network/computational graph
 X_train = tf.constant(train_X_plus_bias, dtype=tf.float32, name="X_train")
@@ -26,7 +34,10 @@ theta = tf.Variable(tf.random_uniform([train_X_plus_bias.shape[1], 1], -1.0, 1.0
 y_predicted = tf.matmul(X_train, theta, name="predictions")
 error = y_predicted - y_train
 root_mean_squared_error = tf.sqrt(tf.reduce_mean(tf.square(error), name="rmse"))
-gradients = 2/train_X_plus_bias.shape[0] * tf.matmul(tf.transpose(X_train), error)
+# manually computing gradients from normal quation
+#gradients = 2/train_X_plus_bias.shape[0] * tf.matmul(tf.transpose(X_train), error)
+# using autodiff/auto differentiation feature of tensorflow 
+gradients = tf.gradients(root_mean_squared_error, [theta])[0]
 training_ops = tf.assign(theta, theta - learning_rate*gradients)
 
 # training and testing  the network
@@ -35,7 +46,7 @@ with tf.Session() as sess:
 
     # network training 
     for epoch in range(epochs):
-        if epoch%50==0:
+        if epoch%1000==0:
             print("Epoch", epoch, "RMSE: ", root_mean_squared_error.eval())
             # print("Errors:", error.eval())
             # print("Transpose input features:", tf.transpose(X_train).eval())
